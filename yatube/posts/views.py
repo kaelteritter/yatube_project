05e8ae8ffc53db2ab7e348ff1f7e34f3129fdc07
kae <me@me>
2025-02-16 00:13:58 +0400

    2

---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.all()[:10]
+    context = {
+            'posts': posts,
+        }
+    return render(request, 'posts/index.html', context)
+
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+            'group': group,
+            'posts': posts,
+        }
+    return render(request, 'group.html', context) 
